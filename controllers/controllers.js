cf697dd90e21373cab5796ff25d78a88dcf790dd
Lea <lea.henry5@hotmail.fr>
2024-05-28 20:44:26 +0100

    Add task 3

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,8 +1,21 @@
 const {selectTopics} = require("../models/models")
+const fs = require("fs")
+
+
 
 exports.getTopics = (req, res) => {
     return selectTopics()
       .then((topics) => {
         res.status(200).send({ topics })
       }) 
+}
+
+exports.getEndpoints = (req, res) => {
+    fs.readFile(__dirname + "/../endpoints.json", "utf-8", (err, data) => {
+        if (err) {
+            console.log(err)
+        } else {
+            res.status(200).send(JSON.parse(data))
+        }
+    })
 }
