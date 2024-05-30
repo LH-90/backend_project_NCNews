3a025e64f711b205b2a3e9f8bee2ae9705cfad4f
Lea <lea.henry5@hotmail.fr>
2024-05-30 17:55:40 +0100

    Add task 8

--- models/models.js
+++ models/models.js
@@ -49,3 +49,10 @@ exports.insertComment = (article_id, username, body) => {
       return result.rows[0]
    })
 }
+
+exports.modifyArticle = (inc_votes, article_id) => {
+   return db.query(`UPDATE articles SET votes = votes + $1 WHERE article_id = $2 RETURNING*;`, [inc_votes, article_id])
+   .then((result) => {
+         return result.rows[0]
+      })
+}
